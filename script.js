7545d264eb0820837ddb01d2af452fb33381bd6f
DHAKSHAYANI T <your_email@example.com>
2026-01-12 00:22:30 +0530

    todolist

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const input = document.getElementById("input");
+    const inputError = document.getElementById("inputError");
+    const listItem = document.getElementById("list-container");
+    const addbtn = document.getElementById("addbtn");
+
+
+
+    addbtn.addEventListener("click", addTask);
+
+    function addTask() {
+
+        if (input.value.trim() === "") {
+            inputError.textContent = "Write the task first";
+            inputError.style.display = "block";
+            return;
+
+        } else {
+            inputError.style.display = "none";
+            let li = document.createElement("li");
+            let textspan = document.createElement("span");
+            textspan.className = "tasktext";
+            textspan.textContent = input.value;
+            li.append(textspan);
+            listItem.appendChild(li);
+
+            let del = document.createElement("span");
+            del.className = "delete"
+            del.textContent = "\uD83D\uDDD1";
+            li.append(del);
+            input.value = "";
+        }
+    }
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addTask();
+        }
+    })
+
+    listItem.addEventListener("click", function (e) {
+
+        if (e.target.classList.contains("tasktext")) {
+            e.target.closest("li").classList.toggle("checked");
+        }
+        else if (e.target.classList.contains("delete")) {
+            e.target.closest("li").remove();
+            return;
+        }
+    })
+})
+
